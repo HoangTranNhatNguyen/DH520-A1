71e322cb68d0f1009ff39756203edf3e7c28e2a6
Cheryl Walker <cherylnancywalker@gmail.com>
2020-09-21 12:58:45 -0600

    Add files via upload
    pseudo code for game

---
+++ game.py
@@ -0,0 +1,29 @@
+Psuedo Code
+
+Print("Let's start with some information about you")
+Print("What's your name: \n")
+name = input
+Print("What's your favourite food?: \n")
+food = input
+Print(f "Ok {name}, you like {food}. Let's go grocery shoppping." )
+
+Question 1: Do you have a mask
+if yes, go to question 2. If no, go to Out_run killer bees
+Question 2: Do you need flour, yeast, toiletpaper or lysol?
+If yes, go to Random Koan generator, If no, go Question 3
+Question 3: Roll for line length (random int), if greater than X, you die in line, if not, go to Success
+
+Random Koan Generator
+CW - code to add in words, then generate random sentences
+
+Roll for length in line
+use rand.int to generate a number. If the number is greater than what a user entered as their max time line line,
+user fails at groceries, if yes, they succedd
+
+Outrun killer hornets - counter to count avoided hornets? If high enough, then win, if not, die
+
+Success
+return printed you got your favourite groceries message
+
+Die/Fail
+return printed 2020 got you, loop back to start 
